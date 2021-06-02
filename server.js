cb38e380ae20724f3c4dd4e2928e6592cd024859
Angel Flores <angelfloreschicago@gmail.com>
2021-06-02 17:46:03 -0500

    added server js file and additional folders and files

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongojs = require('mongojs');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/angelworkouts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Routes
+app.use(require('./routes/apiRoutes'));
+app.use(require('./routes/htmlRoutes'));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
